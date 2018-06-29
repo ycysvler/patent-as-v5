c8db56ee68b5d191955e278e72e43b9dc293fd67
VLER <ycysvler@hotmail.com>
2018-06-30 07:36:52 +0800

    + 增加跨域中间件

--- server/app.js
+++ server/app.js
@@ -6,9 +6,10 @@
 
 const path = require('path');                           // 基础库
 const moment = require('moment');                       // 时间日期库
+const cors = require('koa2-cors');                      // 引用跨域库
 const Koa = require('koa');                             // 引用koa框架
 const bodyparser = require('koa-bodyparser');           // 加载bodyparser中间件
-const static = require('koa-static');                   // 加载静态资源处理
+const koastatic = require('koa-static');                // 加载静态资源处理
 const consuming = require('./middleware/consuming');    // 加载计算耗时中间件
 const logger = require('./utils/logger');               // 引用日志组建
 const loader = require('./utils/loader');               // 路由加载器
@@ -33,7 +34,8 @@ app.on('error', (err, ctx) => {                         // 全局错误处理
         'server error');
 });
 
-app.use(static(path.join(__dirname, '../public')));     // 处理静态资源
+app.use(cors());                                        // 处理跨域
+app.use(koastatic(path.join(__dirname, '../public')));  // 处理静态资源
 app.use(bodyparser());                                  // 使用ctx.body解析中间件
 app.use(consuming);                                     // 计算耗时中间件
 app.use(loader.routes()).use(loader.allowedMethods());  // 加载路由
@@ -41,6 +43,6 @@ app.use(loader.routes()).use(loader.allowedMethods());  // 加载路由
 
 app.listen(config.server.port);                         // 启动http服务
 
-log.info({port: config.server.port}, 'patent v5.0 service is starting at port ' + config.server.port);
+log.info({port: config.server.port}, 'patent v5.0 app services is starting at port ' + config.server.port);
 
 module.exports = app;
